8dd59d7ab2f44029c395d1e6451b4683c5fce002
LucLucero <lucianoxlucero@hotmail.com>
2022-11-08 17:17:22 -0300

    feat: add offline.html to cache

--- sw.js
+++ sw.js
@@ -1,8 +1,19 @@
+const CACHE_KEY = 'appShell-v1';
+
+const assetsToCache = ["offline.html"];
+
+async function cacheStaticAssets(){
+
+    const cache = await caches.open(CACHE_KEY);
+    return cache.addAll(assetsToCache);
+
+}
+
 self.addEventListener('install', event => {
 
     console.log('[Service Worker] Installing service worker...');
+    event.waitUntil(cacheStaticAssets());
     self.skipWaiting();
-    
 });
 
 self.addEventListener('activate', event => {
@@ -16,8 +27,8 @@ self.addEventListener('activate', event => {
 self.addEventListener("fetch", (event) => {
 
 
-    console.log('[Service Worker] Fetching evnt worker');
-    event.respondWidth(fetch(event.request));
+    console.log('[Service Worker] Fetch event worker');
+    event.respondWith(fetch(event.request.url));
 
 
 })
